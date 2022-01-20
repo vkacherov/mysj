aee93b62e4491be08b1d355bba96f93b6cbb0ef1
Vadim Kacherov <vkavherov@gmail.com>
2022-01-20 17:58:02 -0500

    +Stackdriver debug agent

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var debugAgent = require('@google-cloud/debug-agent').start({serviceContext: {enableCanary: false}});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
